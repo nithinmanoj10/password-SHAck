ac425c329726ccfb5879e1da761668ae1d44de2d
nithinmanoj10 <nithinp.manoj@gmail.com>
2023-05-05 01:27:12 +0530

    🌟 All basic functionalities added

--- password_shack/password_shack.py
+++ password_shack/password_shack.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
-from vault import create_vault, delete_vault, list_vaults
+from vault import create_vault, delete_vault, list_vaults, add_to_vault, view_vault
+from input_check import is_valid_add_query, validate_master_password
 
 from rich.pretty import pprint
 
@@ -27,6 +28,19 @@ parser.add_argument(
     default="shack",
 )
 
+parser.add_argument(
+    '-S',
+    '--site_name',
+    help="Name of the site",
+    metavar="site_name"
+)
+
+parser.add_argument(
+    '-U',
+    '--username',
+    help="Username used in the site login",
+    metavar="user_name"
+)
 
 args = parser.parse_args()
 
@@ -42,8 +56,31 @@ def main():
         return
         
     if args.vault_action in ["delete", "d"]:
+        
         delete_vault(args.vault_name)
         return
+    
+    # adding a password
+    if args.vault_action in ["add", "a"]:
+        if not is_valid_add_query(args):
+            return
+        
+        result = validate_master_password(args)
+        
+        if result is not None:
+            add_to_vault(result[0], result[1], args)
+            
+        return
+    
+    # viewing a password
+    if args.vault_action in ["view", "v"]:
+        if not is_valid_add_query(args):
+            return
+        
+        result = validate_master_password(args)
+        
+        if result is not None:
+            view_vault(result[0], result[1], args)
         
         
 main()
